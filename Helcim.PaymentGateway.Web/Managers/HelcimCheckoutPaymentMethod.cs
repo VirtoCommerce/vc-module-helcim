190d4b7a8ca4418ccf69e61cdf16ddb4d4ffde62
konstantin <ksa@virtoway.com>
2018-01-11 16:17:09 +0200

    #1 Payment method basic module structure added

---
+++ Helcim.PaymentGateway.Web/Managers/HelcimCheckoutPaymentMethod.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Specialized;
+using VirtoCommerce.Domain.Payment.Model;
+
+namespace Helcim.PaymentGateway.Web.Managers
+{
+    public class HelcimCheckoutPaymentMethod : PaymentMethod
+    {
+        private const string _paymentMethodModeStoreSetting = "Helcim.PaymentGateway.Mode";
+
+        #region Settings        
+
+        private string ApiMode
+        {
+            get
+            {
+                return GetSetting(_paymentMethodModeStoreSetting);
+            }
+        }
+        
+        #endregion
+
+        public override PaymentMethodType PaymentMethodType
+        {
+            get { return PaymentMethodType.PreparedForm; }
+        }
+
+        public override PaymentMethodGroupType PaymentMethodGroupType
+        {
+            get { return PaymentMethodGroupType.Alternative; }
+        }
+
+        public HelcimCheckoutPaymentMethod() : base("HelcimCheckout")
+        {
+        }
+
+        public override ProcessPaymentResult ProcessPayment(ProcessPaymentEvaluationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override PostProcessPaymentResult PostProcessPayment(PostProcessPaymentEvaluationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override CaptureProcessPaymentResult CaptureProcessPayment(CaptureProcessPaymentEvaluationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RefundProcessPaymentResult RefundProcessPayment(RefundProcessPaymentEvaluationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValidatePostProcessRequestResult ValidatePostProcessRequest(NameValueCollection queryString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override VoidProcessPaymentResult VoidProcessPayment(VoidProcessPaymentEvaluationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Helcim.PaymentGateway.Web/Module.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Helcim.PaymentGateway.Web.Managers;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+using VirtoCommerce.Domain.Payment.Services;
+using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Core.Settings;
+
+namespace Helcim.PaymentGateway.Web
+{
+    public class Module : ModuleBase
+    {
+        private readonly IUnityContainer _container;
+
+        public Module(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public override void Initialize()
+        {
+            var settingsManager = ServiceLocator.Current.GetInstance<ISettingsManager>();
+            Func<HelcimCheckoutPaymentMethod> helcimPaymentMethod = () =>
+            {
+                var paymentMethod = new HelcimCheckoutPaymentMethod();
+                paymentMethod.Name = "Helcim Payment Gateway";
+                paymentMethod.Description = "Helcim payment gateway integration";
+                paymentMethod.LogoUrl = "https://raw.githubusercontent.com/VirtoCommerce/vc-module-helcim/master/Helcim.PaymentGateway.Web/Content/logo.svg";
+                paymentMethod.Settings = settingsManager.GetModuleSettings("Helcim.PaymentGateway");
+                return paymentMethod;
+            };
+
+            var paymentMethodsService = _container.Resolve<IPaymentMethodsService>();
+            paymentMethodsService.RegisterPaymentMethod(helcimPaymentMethod);
+        }
+    }
+}
